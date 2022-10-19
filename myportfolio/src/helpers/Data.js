d239d93bf417dd32d6d63740dc27feb6782fba61
walthercio Almeida <suaagenciainicial@gmail.com>
2022-10-18 22:33:53 -0300

    feat: function to listed projects

--- myportfolio/src/helpers/Data.js
+++ myportfolio/src/helpers/Data.js
@@ -1,8 +1,8 @@
 const data = [
-    {   id: 1,
+    {   id: 0,
         title: 'Star Wars Search Planet',
         image: 'https://github.com/tercioab/StarWars-search-planets/raw/master/Captura%20de%20tela%20de%202022-10-04%2021-51-43.png',
-        Habilidades: {
+        habilidades: {
          habilidade1:'Context API do React para gerenciar estado.',
          habilidade2:'React Hook useState',
          habilidade3:'React Hook useEffect',
@@ -13,10 +13,10 @@ const data = [
     },
 
     {
-        id: 2,
+        id: 1,
         title: 'Trybewallet',
             image:'https://github.com/tercioab/Trybewallet-/raw/walthercio-sd-023-b-project-trybewallet/imagensReadme/Captura%20de%20tela%20de%202022-09-12%2002-02-19.png',
-        Habilidades: {
+        habilidades: {
             habilidade1: 'Criar um store Redux em aplicações React',
             habilidade2: 'Criar reducers no Redux em aplicações React',
             habilidade3: 'Criar actions no Redux em aplicações React',
--- myportfolio/src/pages/Projetos.js
+++ myportfolio/src/pages/Projetos.js
@@ -1,8 +1,31 @@
 import React from "react";
+import data from "../helpers/Data";
 
-export default function Projetos () {
-    return (
-       <h1>projetos</h1>
-    )
+export default function Projetos() {
+
+	const createLi = (index) => {
+		const habilidades = Object.entries(data[index].habilidades)
+			.filter(t => t[0].includes("habilidade"))
+			.filter(d => d[1] !== " ");
+		const lis = [];
+		for (let i = 0; i < habilidades.length; i += 1) {
+			lis.push(<div key={i}>{`${habilidades[i][1]}`}</div>);
+		}
+		return lis;
+	};
+
+	
+
+	return (
+		<>
+			<h1>projetos</h1>
+            {data.map(({ id, title }) =>
+                <div>
+                    <h3>{title}</h3>
+                    {createLi(id)}
+                </div>
+                
+            )}
+		</>
+	);
 }
-
